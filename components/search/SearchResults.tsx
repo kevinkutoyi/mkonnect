446fd8d9265a010de6a9652a95bed3d402b7e660
Kevin <amoraghali@gmail.com>
2026-06-22 21:20:45 +0300

    fix: remove useSearchParams from server component, ProfileHero broken images, admin users API, Google verification

--- components/search/SearchResults.tsx
+++ components/search/SearchResults.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import { MasseuseCard } from "./MasseuseCard";
 import { SearchX } from "lucide-react";
-import { useSearchParams } from "next/navigation";
 
 interface Props {
   masseuses: any[];
